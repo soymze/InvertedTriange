9b6e73b0b1cee802743feddf7ebb07357faa8dd7
soymze <soylmze@gmail.com>
2024-01-24 16:59:01 +0300

    şşş

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int lines;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Satır sayısını giriniz:");
+        lines=sc.nextInt();
+
+        for(int i = 0; i<lines; i++){
+            for(int k = (2*(lines-i))-1; k>=1; k--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
 }
